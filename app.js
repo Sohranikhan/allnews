59493d3638a7e39badded60f02339ef4d9877c8a
Amir Sohail <104058975+Sohranikhan@users.noreply.github.com>
2023-01-27 23:43:48 -0800

    pakistani news

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 let main = document.querySelector(".main");
- 
+ let getdat1 = async()=>{ 
+
+    let data = await fetch('https://nodeapigithubio-production.up.railway.app/')
+   let response = await data.json()
+   
+   resimgall1(response)
+   }
+   const resimgall1=(response)=>{
+       response.forEach(element => {
+           
+       let card = document.createElement("div");
+       card.classList.add('card')
+       card.innerHTML=`
+       <img src="${element.src}" alt="">
+       <div class="flex">
+       <h1>${element.parag}</h1>
+       <h3 class="para">${element.head}</h3>
+      
+   
+   </div>
+      `
+       main.appendChild(card);
+   });
+   }
+       
+   
+   getdat1()
 let getdat = async()=>{ 
 
  let data = await fetch('https://worldnews-production.up.railway.app/')
