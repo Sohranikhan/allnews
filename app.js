b66a26718ec3380580c1424ea67778a3f691e512
Amir Sohail <104058975+Sohranikhan@users.noreply.github.com>
2023-01-28 00:30:28 -0800

    more geo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let main = document.querySelector(".main");
  let getdat1 = async()=>{ 
 
-    let data = await fetch('https://nodeapigithubio-production.up.railway.app/')
+    let data = await fetch('https://thegeo-production.up.railway.app/')
    let response = await data.json()
    
    resimgall1(response)
@@ -14,8 +14,8 @@ let main = document.querySelector(".main");
        card.innerHTML=`
        <img src="${element.src}" alt="">
        <div class="flex">
-       <h1>${element.parag}</h1>
-       <h3 class="para">${element.head}</h3>
+       <h1>${element.heading}</h1>
+       <h3 class="para">${element.ptag}</h3>
       
    
    </div>
@@ -26,6 +26,28 @@ let main = document.querySelector(".main");
        
    
    getdat1()
+ let getdat2 = async()=>{ 
+
+        let data = await fetch('https://thegeo-production.up.railway.app/more')
+       let response = await data.json()
+       
+       resimgall2(response)
+       }
+       const resimgall2=(response)=>{
+           response.forEach(element => {
+               
+           let card = document.createElement("div");
+           card.classList.add('card')
+           card.innerHTML=`
+           <img src="${element.imgurl}" alt="">
+           <div class="flex">
+           <h2>${element.heading}</h2>
+           <p class="para">${element.date}</p>
+       </div>
+          `
+           main.appendChild(card);
+       });}
+         getdat2()
 let getdat = async()=>{ 
 
  let data = await fetch('https://worldnews-production.up.railway.app/')
